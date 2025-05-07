cea6c264c9878783b7ae274323687d94ec2366c6
apuchnin <apuchnin@ozon.ru>
2025-05-07 03:08:44 +0300

    1296

---
+++ 1296hypertransition/1296hypertransition/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1296hypertransition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var consistency = new int[n];
+            for (int i = 0; i < consistency.Length; i++)
+            {
+                consistency[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(FindBestConsistenty(consistency));
+        }
+        static int FindBestConsistenty(int[] consistency)
+        {
+            if (consistency.Length == 0)
+                return 0;
+
+            int maxSum = 0; int currentSum = 0;
+            foreach (int number in consistency)
+            {
+                currentSum = Math.Max(number, currentSum + number);
+                maxSum = Math.Max(maxSum, currentSum);
+               
+            }
+            return maxSum;
+        }
+    }
+}
